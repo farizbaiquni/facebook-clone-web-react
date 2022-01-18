7b556ba209e4cc2b2bcf88c726bbe2e249fb3ac8
Fariz Baiquni <farizbaiquni@gmail.com>
2022-01-19 04:16:29 +0700

    add firebase library

---
+++ src/lib/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTiS9LDcvazB-fPZzVdKscFDahkriH5OU",
+  authDomain: "facebook-clone-react-c5dca.firebaseapp.com",
+  projectId: "facebook-clone-react-c5dca",
+  storageBucket: "facebook-clone-react-c5dca.appspot.com",
+  messagingSenderId: "386456420035",
+  appId: "1:386456420035:web:a7b2bdd67238d1928b6c76",
+  measurementId: "G-R8V5ZH2X0S"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase
