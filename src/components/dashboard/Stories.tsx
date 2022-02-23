9f5dbd4a06e607fcf2083356e14adf273c186cd6
Fariz Baiquni <farizbaiquni@gmail.com>
2022-02-23 15:16:28 +0700

    improve story card hover effect

--- src/components/dashboard/Stories.tsx
+++ src/components/dashboard/Stories.tsx
@@ -25,7 +25,7 @@ export default function Stories() {
 }
 
 const LastStory = () => (
-  <div className=' relative'>
+  <div className=' relative opacity-95 hover:opacity-100'>
     <img src={process.env.PUBLIC_URL + './example_story.jpg'} alt="" className=' w-full h-full rounded-md object-cover absolute' />
     <h1 className=' absolute text-white w-full text-center bottom-1 text-sm font-semibold break-all px-2'>Muhammad Fariz Baiquni</h1>
     <img src={process.env.PUBLIC_URL + './profile.jpg'} alt="" className=' absolute left-2 top-2 h-10 w-10 rounded-full border-4 border-blue-600' />
--- src/components/dashboard/Story.tsx
+++ src/components/dashboard/Story.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Story() {
   return (
-      <div className=' relative'>
+      <div className=' relative cursor-pointer opacity-95 hover:opacity-100'>
         <img src={process.env.PUBLIC_URL + './example_story.jpg'} alt="" className=' w-full h-full rounded-md object-cover absolute' />
         <h1 className=' absolute text-white w-full text-center bottom-1 text-sm font-semibold break-all px-2'>Muhammad Fariz Baiquni</h1>
         <img src={process.env.PUBLIC_URL + './profile.jpg'} alt="" className=' absolute left-2 top-2 h-10 w-10 rounded-full border-4 border-blue-600' />
